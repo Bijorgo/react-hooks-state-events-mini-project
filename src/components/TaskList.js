8697167b06b3d49992988c19a2d14ae6f6d7a130
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2024-12-12 22:47:34 -0700

    display tasks and delete button

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,8 +1,26 @@
 import React from "react";
+import Task from "./Task";
+import { useState } from "react";
 
-function TaskList() {
+function TaskList({tasks}) {
+  const [ taskList, setTaskList] = useState(tasks)
+
+  const removeTask = (taskId) => {
+    setTaskList(taskList.filter(task => task.id !== taskId))
+  };
+
   return (
     <div className="tasks">
+      {taskList.map( task => (
+        <li key= {task.id}>
+          <Task 
+          id={task.id}
+          text={task.text} 
+          category={task.category}
+          onDelete={removeTask}
+          />
+        </li>
+      ))}
       {/* display a list of tasks using Task component */}
     </div>
   );
